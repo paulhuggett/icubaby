2e7cb7f67eded66ee41b6a20298c3ef26d986348
Paul Bowen-Huggett <paulhuggett@mac.com>
2022-11-19 16:07:20 +0100

    Add shell test for UTF-32.

---
+++ unittests/test_u32.cpp
@@ -0,0 +1,9 @@
+#include "icubaby/icubaby.hpp"
+
+static_assert (std::is_same_v<icubaby::t32_8 ::input_type, char32_t> &&
+               std::is_same_v<icubaby::t32_8 ::output_type, char8_t>);
+static_assert (std::is_same_v<icubaby::t32_16::input_type, char32_t> &&
+               std::is_same_v<icubaby::t32_16::output_type, char16_t>);
+static_assert (std::is_same_v<icubaby::t32_32::input_type, char32_t> &&
+               std::is_same_v<icubaby::t32_32::output_type, char32_t>);
+
