463022a11e35eddab35444314bae471151f31143
Paul Bowen-Huggett <paulhuggett@mac.com>
2022-11-28 10:09:59 +0100

    Suppress warning about a static_assert.

--- unittests/test_u32_8.cpp
+++ unittests/test_u32_8.cpp
@@ -8,6 +8,7 @@ static_assert (std::is_same_v<icubaby::t32_8 ::input_type, char32_t> &&
                std::is_same_v<icubaby::t32_8 ::output_type, icubaby::char8>);
 static_assert (std::is_same_v<icubaby::t32_16::input_type, char32_t> &&
                std::is_same_v<icubaby::t32_16::output_type, char16_t>);
+// NOLINTNEXTLINE(misc-redundant-expression)
 static_assert (std::is_same_v<icubaby::t32_32::input_type, char32_t> &&
                std::is_same_v<icubaby::t32_32::output_type, char32_t>);
 
