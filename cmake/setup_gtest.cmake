4ddd6e9dc6e3ac5e66af312f04f39afe0c4be620
Paul Bowen-Huggett <paulhuggett@mac.com>
2022-12-08 09:29:23 +0100

    Remove the lib directory. Move gtest setup to its own module.

---
+++ cmake/setup_gtest.cmake
@@ -0,0 +1,51 @@
+# MIT License
+#
+# Copyright (c) 2022 Paul Bowen-Huggett
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+
+# Tell gtest to link against the "Multi-threaded Debug DLL runtime
+# library" on Windows.
+set (gtest_force_shared_crt ON CACHE BOOL "Always use msvcrt.dll")
+add_subdirectory ("${icubaby_project_root}/googletest")
+
+function (setup_gtest)
+  foreach (target gtest gmock gmock_main gtest_main)
+    set_target_properties (${target} PROPERTIES
+      CXX_STANDARD ${standard}
+      CXX_STANDARD_REQUIRED Yes
+      CXX_EXTENSIONS No
+    )
+    set (gclang_options -Wno-implicit-int-float-conversion)
+    if (LIBCXX)
+      list (APPEND gclang_options -stdlib=libc++)
+    endif ()
+    set (gintelllvm_options ${gclang_options} -Wno-tautological-constant-compare)
+
+    target_compile_definitions (${target} PUBLIC GTEST_REMOVE_LEGACY_TEST_CASEAPI_=1)
+    target_compile_options (${target} PRIVATE
+      $<$<OR:$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:AppleClang>>:${gclang_options}>
+      $<$<CXX_COMPILER_ID:IntelLLVM>:${gintelllvm_options}>
+    )
+    target_link_options (${target} PRIVATE
+      $<$<OR:$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:AppleClang>>:${gclang_options}>
+      $<$<CXX_COMPILER_ID:IntelLLVM>:${gintelllvm_options}>
+    )
+  endforeach()
+endfunction (setup_gtest)
+
