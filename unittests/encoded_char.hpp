2553e4435f379cd47c94b0367008cffae692a004
Paul Bowen-Huggett <paulhuggett@mac.com>
2024-01-14 13:03:44 +0100

    Update the copyright year.

--- unittests/encoded_char.hpp
+++ unittests/encoded_char.hpp
@@ -1,6 +1,6 @@
 // MIT License
 //
-// Copyright (c) 2022 Paul Bowen-Huggett
+// Copyright (c) 2022-2024 Paul Bowen-Huggett
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -19,6 +19,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
+
 #ifndef UNITTESTS_ENCODED_CHAR_HPP
 #define UNITTESTS_ENCODED_CHAR_HPP
 
