1c4c3797f97eb657fa6c3145cf9d41545ea24223
Paul Bowen-Huggett <paulhuggett@mac.com>
2024-03-01 18:53:07 +0100

    Resolve warnings in the "manual bytes to UTF-8" example.

--- examples/manual_bytes_to_utf8.cpp
+++ examples/manual_bytes_to_utf8.cpp
@@ -1,6 +1,8 @@
-#include <iostream>
-
 #include "icubaby/icubaby.hpp"
+
+#include <iterator>
+#include <iostream>
+#include <vector>
 
 int main () {
   // The bytes to be converted. An array here, but this could obviously come from any source such as user input, a
@@ -13,8 +15,11 @@ int main () {
                                 std::byte{0x00}, std::byte{'l'},  std::byte{0x00}, std::byte{'d'},  std::byte{0x00},
                                 std::byte{'\n'}};
 
-  // An output iterator: where the UTF-8 output will be written.
-  std::ostream_iterator<std::uint_least8_t> out{std::cout};
+  // A vector to contain the UTF-8 output.
+  std::vector<icubaby::char8> output;
+
+  // An output iterator that will append each UTF-8 code unit to the `output` vector.
+  auto out_it = std::back_inserter (output);
 
   // The transcoder instance. We consume bytes (indicating that the transcoder must decide on the input encoding)
   // and emit icubaby::char8 (UTF-8).
@@ -22,9 +27,14 @@ int main () {
 
   // Call the transcoder for each source byte. Output goes to the 'out' output iterator.
   for (auto b : input) {
-    out = transcode (b, out);
+    out_it = transcode (b, out_it);
   }
 
   // Tell the transoder that it should have received a complete code point. This always happens at the end of the input.
-  (void)transcode.end_cp (out);
+  (void)transcode.end_cp (out_it);
+
+  // Write the output to the console. This example sticks to the ASCII subset of code point, so this should work on most terminals!
+  for (auto c: output) {
+    std::cout << static_cast<char> (c);
+  }
 }
