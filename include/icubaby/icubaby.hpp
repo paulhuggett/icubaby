fd26d1cec230ac2e6512ae36f008f26383e32c80
Paul Bowen-Huggett <paulhuggett@mac.com>
2025-03-09 19:48:38 +0100

    Add a default class to the byte transcoder state switch.

--- include/icubaby/icubaby.hpp
+++ include/icubaby/icubaby.hpp
@@ -1362,6 +1362,8 @@ public:
 
     case states::run_32be_byte3:
     case states::run_32le_byte3: dest = this->run32 (value, dest); break;
+
+    default: assert (false && "Unknown byte transcoder state");
     }
     return dest;
   }
